97747614249977adb6fefc0953d68ab8dfbac71a
Sudeep-Sharma0-0 <sharma.sudip1122@gmail.com>
2021-07-10 21:17:20 +0545

    Added Images Styles and Colors

--- src/main/java/com/javafx/project/App.java
+++ src/main/java/com/javafx/project/App.java
@@ -1,48 +1,49 @@
 package com.javafx.project;
 
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.Text;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
+import javafx.util.*;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Stop[] stops = new Stop[] { new Stop(0, Color.GREEN), new Stop(1, Color.BLUE)};
-        LinearGradient linear = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, stops);
+        FadeTransition fade = new FadeTransition();
 
+        Text empty = new Text();
         Text username = new Text();
         Text password = new Text();
-        TextField txtfield = new TextField();
+        TextField userfield = new TextField();
         PasswordField passfield = new PasswordField();
 
-        username.setText("Username: ");
-        password.setText("Password: ");
+        fade.setDuration(Duration.millis(5000));
+        fade.setFromValue(0.1);
+        fade.setToValue(1.0);
+        fade.play();
+
+        username.setText("Username ");
+        password.setText("Password ");
+        empty.setText("");
+        username.setStyle("-fx-background-image: url('')");
         username.setFont(Font.font("Montserrat", FontPosture.REGULAR, 25));
         password.setFont(Font.font("Montserrat", FontPosture.REGULAR, 25));
-        username.setFill(linear);
 
-        VBox root = new VBox();
-        root.getChildren().add(username);
-        root.getChildren().add(password);
+        GridPane root = new GridPane();
         root.setAlignment(Pos.CENTER);
-        root.setId("pane");
+        root.setStyle("-fx-background-color: linear-gradient(rgba(199,55,255,1) 0%, rgba(0,218,244,1) 100%)");
+        root.addRow(0, username, userfield);
+        root.addRow(1, empty);
+        root.addRow(2, password, passfield);
 
         Scene scene = new Scene(root);
-        scene.setFill(Color.RED);
 
         primaryStage.setScene(scene);
         primaryStage.setWidth(1280);
