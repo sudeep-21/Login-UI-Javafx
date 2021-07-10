f741ca311e45d5c2c6ad9fcc26d6bdfe0d6a3a90
Sudeep-Sharma0-0 <sharma.sudip1122@gmail.com>
2021-07-11 02:56:40 +0545

    Completed Login UI

--- src/main/java/com/javafx/project/App.java
+++ src/main/java/com/javafx/project/App.java
@@ -2,57 +2,93 @@ package com.javafx.project;
 
 import javafx.animation.*;
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.*;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 import javafx.util.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        FadeTransition fade = new FadeTransition();
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        TranslateTransition translate = new TranslateTransition();
+
+        Image image = new Image("logo.png");
+        ImageView imgView = new ImageView(image);
+        imgView.setFitWidth(500);
+        imgView.setFitHeight(300);
+        imgView.setTranslateY(-400);
 
         Text empty = new Text();
-        Text username = new Text();
-        Text password = new Text();
+        Label username = new Label();
+        Label password = new Label();
+        Label message = new Label();
         TextField userfield = new TextField();
         PasswordField passfield = new PasswordField();
+        Button submit = new Button("Submit");
 
-        fade.setDuration(Duration.millis(5000));
-        fade.setFromValue(0.1);
-        fade.setToValue(1.0);
-        fade.play();
+        translate.setDuration(Duration.seconds(3.3));
+        translate.setByY(400);
+        translate.setNode(imgView);
+        translate.play();
 
         username.setText("Username ");
         password.setText("Password ");
-        empty.setText("");
-        username.setStyle("-fx-background-image: url('')");
-        username.setFont(Font.font("Montserrat", FontPosture.REGULAR, 25));
-        password.setFont(Font.font("Montserrat", FontPosture.REGULAR, 25));
+        submit.setTranslateY(30);
+        message.setText("");
+        message.setId("message");
+        message.setTranslateY(50);
 
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setStyle("-fx-background-color: linear-gradient(rgba(199,55,255,1) 0%, rgba(0,218,244,1) 100%)");
-        root.addRow(0, username, userfield);
-        root.addRow(1, empty);
-        root.addRow(2, password, passfield);
+        VBox mroot = new VBox();
 
-        Scene scene = new Scene(root);
+        VBox lroot = new VBox();
+        lroot.setId("pane");
+        lroot.setAlignment(Pos.BASELINE_CENTER);
+        lroot.setFillWidth(false);
+        lroot.getChildren().addAll(imgView, username, userfield, password, passfield, submit, message);
 
-        primaryStage.setScene(scene);
+        Scene login = new Scene(lroot);
+        Scene main = new Scene(mroot);
+
+        submit.setCursor(Cursor.HAND);
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String uname = userfield.getText().toString();
+                String pass = passfield.getText().toString();
+
+                if (uname.equals("admin") && pass.equals("admin")) {
+                    message.setText("Login Successful!!");
+                    message.setStyle("-fx-text-fill: lime;");
+                    primaryStage.setScene(main);
+                } else {
+                    message.setText("Incorrect Crenditials!!");
+                    message.setStyle("-fx-text-fill: #9E0000;");
+                }
+            }
+        });
+
+        login.getStylesheets().addAll(this.getClass().getResource("/styles/styles.css").toExternalForm());
+        primaryStage.setScene(login);
         primaryStage.setWidth(1280);
         primaryStage.setHeight(720);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Application.launch();
+        launch(args);
     }
 
 }
