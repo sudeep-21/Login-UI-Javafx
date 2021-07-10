c2d9bce475ca3e179cbbbfeb3abe4d11c745daff
Sudeep-Sharma0-0 <sharma.sudip1122@gmail.com>
2021-07-10 19:01:52 +0545

    Initial Commit

--- src/main/java/com/javafx/project/App.java
+++ src/main/java/com/javafx/project/App.java
@@ -1,22 +1,13 @@
 package com.javafx.project;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        
     }
 
     public static void main(String[] args) {
